9bc7d4acd443e3e7f09783f22da541d9461ed99d
emma <du.changkun@cloud-ace.com>
2024-01-22 16:17:46 +0800

    AC Day 6-Part_One

--- src/day06.zig
+++ src/day06.zig
@@ -11,9 +11,68 @@ const gpa = util.gpa;
 const data = @embedFile("data/day06.txt");
 
 pub fn main() !void {
-    
+    const part_one = try multiWays(data);
+    std.debug.print("part_one={d}\n", .{part_one});
 }
 
+fn multiWays(doc: []const u8) !usize {
+    var buf: [1024]u8 = undefined;
+    var stream = std.io.fixedBufferStream(doc);
+    var time_buf: [1024]usize = undefined;
+    var time_index: usize = 0;
+    var distance_buf: [1024]usize = undefined;
+    var distance_index: usize = 0;
+    var is_time_line = true;
+    while (true) {
+        const line_maybe = try stream.reader().readUntilDelimiterOrEof(&buf, '\n');
+        if (line_maybe) |line| {
+            if (is_time_line) {
+                is_time_line = false;
+                var it_time = std.mem.splitSequence(u8, std.mem.trimLeft(u8, line, "Time:      "), " ");
+                while (it_time.next()) |time_str| {
+                    if (std.mem.eql(u8, time_str, "")) {
+                        continue;
+                    }
+                    time_buf[time_index] = try std.fmt.parseInt(usize, time_str, 10);
+                    time_index += 1;
+                }
+            } else {
+                var it_distance = std.mem.splitSequence(u8, std.mem.trimLeft(u8, line, "Distance:  "), " ");
+                while (it_distance.next()) |distance_str| {
+                    if (std.mem.eql(u8, distance_str, "")) {
+                        continue;
+                    }
+                    distance_buf[distance_index] = try std.fmt.parseInt(usize, distance_str, 10);
+                    distance_index += 1;
+                }
+            }
+        } else {
+            break;
+        }
+    }
+    var multi: usize = 1;
+    for (0..time_index) |t_i| {
+        var count: usize = 0;
+        for (0..time_buf[t_i] + 1) |i| {
+            if (i * (time_buf[t_i] - i) > distance_buf[t_i]) {
+                count += 1;
+            }
+        }
+        if (count == 0) {
+            count = 1;
+        }
+        multi *= count;
+    }
+    return multi;
+}
+
+test "part one example" {
+    const doc =
+        \\Time:      7  15   30
+        \\Distance:  9  40  200
+    ;
+    try std.testing.expect(try multiWays(doc) == 288);
+}
 // Useful stdlib functions
 const tokenizeAny = std.mem.tokenizeAny;
 const tokenizeSeq = std.mem.tokenizeSequence;
